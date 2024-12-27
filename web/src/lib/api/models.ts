98770235fde7226b1336923b4b7b144bec9ff078
varma02 <varma02.marci@gmail.com>
2024-12-27 19:25:38 +0000

    frontend api models

--- web/src/lib/api/models.ts
+++ web/src/lib/api/models.ts
@@ -1,23 +1,70 @@
-import { Employee } from "../../../../server/src/database/models";
+import { Class, Employee, Invite, RelationWorkedAt } from "../../../../server/src/database/models";
 
 export interface BaseResponse {
   code: string;
   message: string;
-  data?: any;
 }
 
-export interface ErrorResponse extends BaseResponse {
-  data: undefined;
-}
+export interface ErrorResponse
+extends BaseResponse {}
 
-export interface LoginResponse extends BaseResponse {
+export interface LoginResponse
+extends BaseResponse {
   data: {
     token: string;
     employee: Employee;
   };
 }
 
-export interface MeResponse extends BaseResponse {
+export interface RegisterResponse
+extends BaseResponse {}
+
+export interface ClearSessionsResponse
+extends BaseResponse {}
+
+export interface MeResponse
+extends BaseResponse {
+  data: {
+    employee: Employee;
+  };
+}
+
+export interface UpdateUserResponse
+extends BaseResponse {
+  data: {
+    user: Employee;
+  }
+}
+
+
+export interface InviteEmployeeResponse
+extends BaseResponse {
+  data: {
+    invite: Invite;
+  };
+}
+
+export interface AllEmployeesResponse
+extends BaseResponse {
+  data: {
+    employees: Employee[];
+  };
+}
+
+export interface SingleEmployeeResponse
+extends BaseResponse {
+  data: {
+    employee: Employee;
+    unpaid_work?: RelationWorkedAt[];
+    classes?: Class[];
+  };
+}
+
+export interface RemoveEmployeeResponse
+extends BaseResponse {}
+
+export interface UpdateEmployeeResponse
+extends BaseResponse {
   data: {
     employee: Employee;
   };

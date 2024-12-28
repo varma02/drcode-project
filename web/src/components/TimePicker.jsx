9df67ba947369e68004efb7fe1a1af305dcd1e10
MaalnaKeX <75688160+MaalnaKeX@users.noreply.github.com>
2024-12-29 00:08:28 +0100

    Create TimePicker.jsx

---
+++ web/src/components/TimePicker.jsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+import { useState } from "react"
+import { setHours, setMinutes } from "date-fns"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+
+export const TimePicker = ({date, setDate, className, label}) => {
+  const [timeValue, setTimeValue] = useState(`${date.getHours()}:${date.getMinutes() < 10 ? "0"+date.getMinutes() : date.getMinutes()}`)
+
+  const handleTimeChange = (e) => {
+    const time = e.target.value
+    if (!date) {
+      setTimeValue(time)
+      return
+    }
+    const [hours, minutes] = time.split(":").map((str) => parseInt(str, 10))
+    const newSelectedDate = setHours(setMinutes(date, minutes), hours)
+    setDate(newSelectedDate)
+    setTimeValue(time)
+  }
+
+  return (
+    <div className="flex flex-col gap-2">
+      <p>{label}</p>
+      <Input type="time" className={`bg-transparent outline-none w-full text-center p-1 ${className}`} value={timeValue} onChange={handleTimeChange} />
+    </div>
+  )
+}

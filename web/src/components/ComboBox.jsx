ab3ad2cc9287abe998787173afade81f81483de3
MaalnaKeX <75688160+MaalnaKeX@users.noreply.github.com>
2024-12-28 16:36:09 +0100

    Added Combobox

---
+++ web/src/components/ComboBox.jsx
@@ -0,0 +1,79 @@
+import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { useState } from "react"
+
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+]
+
+export function Combobox({data, displayName, placeholder = "Válassz...", value, setValue}) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          { value ? data.find(e => e[displayName] === value)?.[displayName] : placeholder }
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder={"Keresés..."} />
+          <CommandList>
+            <CommandEmpty>Nincs Találat.</CommandEmpty>
+            <CommandGroup>
+              {data.map((e) => (
+                <CommandItem
+                  key={e.name}
+                  value={e.name}
+                  onSelect={(currentValue) => {
+                    console.log(currentValue)
+                    setValue(currentValue === value ? "" : currentValue)
+                    setOpen(false)
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === e.name ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {e.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}

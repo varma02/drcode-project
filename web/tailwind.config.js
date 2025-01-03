8ad84e5f6629cba6e13349b26defe17b5438a630
MaalnaKeX <75688160+MaalnaKeX@users.noreply.github.com>
2025-01-03 20:21:38 +0100

    Update tailwind.config.js

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -61,7 +61,12 @@ export default {
   				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
   				border: 'hsl(var(--sidebar-border))',
   				ring: 'hsl(var(--sidebar-ring))'
-  			}
+  			},
+				code: {
+					green: "#99cc33",
+					gray: "#4d4d4d",
+					orange: "#c2500a"
+				}
   		}
   	}
   },

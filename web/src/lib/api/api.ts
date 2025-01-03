1630357dce955dfcdc4f63ab2ee5d8a730f89452
MaalnaKeX <75688160+MaalnaKeX@users.noreply.github.com>
2025-01-03 19:52:20 +0100

    Update api.ts

--- web/src/lib/api/api.ts
+++ web/src/lib/api/api.ts
@@ -4,3 +4,15 @@ import { API_URL } from "./constants";
 export async function getAllEmployees(token:string) {
   return (await axios.get(API_URL + "/employee/all", {headers: {Authorization: `Bearer ${token}`}, timeout: 2000})).data;
 }
+
+export async function getEmployee(token: string, id: string) {
+  return (await axios.get(API_URL + "/employee/"+id, {headers: {Authorization: `Bearer ${token}`}, timeout: 2000})).data;
+}
+
+export async function getAllGroups(token: string) {
+  return (await axios.get(API_URL + "/group/all", {headers: {Authorization: `Bearer ${token}`}, timeout: 2000})).data;
+}
+
+export async function getGroup(token: string, id: string) {
+  return (await axios.get(API_URL + "/group/"+id, {headers: {Authorization: `Bearer ${token}`}, timeout: 2000})).data;
+}

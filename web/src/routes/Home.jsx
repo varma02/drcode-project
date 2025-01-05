9689084ea477c0ffe3cbe1aee8618ac68af868ea
varma02 <varma02.marci@gmail.com>
2025-01-05 20:49:58 +0000

    dashboard design

--- web/src/routes/Home.jsx
+++ web/src/routes/Home.jsx
@@ -2,10 +2,11 @@ import { AssignmentMessage, NotificationMessage } from '@/components/Messages'
 import DataTable from '@/components/DataTable'
 import { Button } from '@/components/ui/button'
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
-import { Folder, ArrowUpDown, BookOpen, Clock, MapPin, User, User2, ArrowRight } from 'lucide-react'
+import { Folder, ArrowUpDown, BookOpen, Clock, MapPin, User, User2, ArrowRight, ArrowUp, ArrowDown, Play } from 'lucide-react'
 import React from 'react'
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import { ToggleButton } from '@/components/ToggleButton'
+import { Textarea } from '@/components/ui/textarea'
 
 export const Home = () => {
 
@@ -36,42 +37,49 @@ export const Home = () => {
       grade: 5,
       status: "success",
       name: "UserName1",
+      subject: "Scratch"
     },
     {
       id: "3u1reuv4",
       grade: 5,
       status: "success",
       name: "UserName2",
+      subject: "Scratch"
     },
     {
       id: "derv1ws0",
       grade: 5,
       status: "processing",
       name: "UserName3",
+      subject: "Scratch"
     },
     {
       id: "5kma53ae",
       grade: 4,
       status: "success",
       name: "UserName4",
+      subject: "WeDo"
     },
     {
       id: "bhqecj4p",
       grade: 4,
       status: "failed",
       name: "UserName5",
+      subject: "Scratch"
     },
     {
       id: "bhqecj4p",
       grade: 4,
       status: "failed",
       name: "UserName6",
+      subject: "Scratch"
     },
     {
       id: "bhqecj4p",
       grade: 4,
       status: "failed",
       name: "UserName7",
+      subject: "WeDo"
     },
   ]
   
@@ -86,11 +94,27 @@ export const Home = () => {
             onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
           >
             {column.columnDef.displayName}
-            <ArrowUpDown />
+            {!column.getIsSorted() ? <ArrowUpDown /> 
+            : column.getIsSorted() === "asc" ? <ArrowDown /> : <ArrowUp />}
           </Button>
         )
       },
-      cell: ({ row }) => <div className="lowercase">{row.getValue("name")}</div>,
+    },
+    {
+      displayName: "Kurzus",
+      accessorKey: "subject",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            {column.columnDef.displayName}
+            {!column.getIsSorted() ? <ArrowUpDown /> 
+            : column.getIsSorted() === "asc" ? <ArrowDown /> : <ArrowUp />}
+          </Button>
+        )
+      },
     },
     {
       displayName: "Osztály",
@@ -111,64 +135,63 @@ export const Home = () => {
   ]
 
   return (
-    <div className='flex flex-col md:grid grid-cols-2 h-full grid-rows-2 gap-4 overflow-y-hidden'>
-      <div className='bg-primary-foreground rounded-xl p-4 row-span-2 h-full'>
-        <h2 className='text-center mb-4'>Következő óra</h2>
+    <div className='flex flex-col md:grid grid-cols-2 h-full grid-rows-3 gap-4 overflow-y-hidden'>
+      <div className='bg-primary-foreground rounded-xl p-4 row-span-3 h-full'>
+        <h2 className='md:text-left text-center mb-4'>Következő óra</h2>
         <div className='flex gap-4 w-full h-full flex-col'>
-          <Card className="max-w-[35rem] w-full h-max">
-            <CardHeader>
-              <div className="flex items-center gap-4">
-                <img src="https://seeklogo.com/images/S/scratch-cat-logo-7F652C6253-seeklogo.com.png" className='max-w-12 object-cover object-center' />
-                <p className='font-bold text-xl'>Scratch, WeDo</p>
-                <span className='flex gap-1 items-center opacity-75 ml-auto'>
-                  <User2 width={20} />
-                  <p className='font-bold'>6</p>
-                </span>
-              </div>
-            </CardHeader>
-            <CardContent>
-              <div className='flex justify-between'>
-                <span className='flex gap-1 items-center opacity-75'>
-                  <ArrowRight width={16} />
-                  <p className='font-bold'>Arany Iskola</p>
-                </span>
-                <span className='flex gap-1 items-center opacity-75'>
-                  <Clock width={16} />
-                  <p className='font-bold'>16:00</p>
-                </span>
-              </div>
-            </CardContent>
-          </Card>
-          {/* <Card className="w-full h-full">
-            <CardContent className="mt-6">
-              <div className='flex items-center gap-4'>
-                <img src="https://seeklogo.com/images/S/scratch-cat-logo-7F652C6253-seeklogo.com.png" className='max-w-36 object-cover object-center' />
-                <div className='flex flex-col gap-2'>
-                  <div className='flex gap-2'><BookOpen /> <p>Scratch</p></div>
-                  <div className='flex gap-2'><Clock /> <p>Hétfő 16:00</p></div>
-                  <div className='flex gap-2'><MapPin />  <p>Arany Iskola</p></div>
-                  <div className='flex gap-2'><User2 />  <p>6</p></div>
+          <Card className="w-full h-36 flex">
+            <img src="https://seeklogo.com/images/S/scratch-cat-logo-7F652C6253-seeklogo.com.png" className='object-cover object-center w-32 p-4 pr-0' />
+            <div className='flex flex-col'>
+              <CardHeader className="pb-4">
+                <div className="flex items-center gap-4">
+                  <p className='font-bold text-xl'>Scratch haladó, Lego WeDo 2.0</p>
                 </div>
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button className="w-full font-bold">Segédlet</Button>
-            </CardFooter>
-          </Card> */}
+              </CardHeader>
+              <CardContent>
+                <div className='flex gap-6'>
+                  <span className='flex gap-2 items-center opacity-75'>
+                    <MapPin width={22} />
+                    <p className='font-bold'>
+                      Arany János Általános Iskola
+                      <br />
+                      <span className="opacity-50">6000 Kecskemét, Lunkányi János u.</span>
+                    </p>
+                  </span>
+                  <span className='flex gap-2 items-center opacity-75'>
+                    <User2 width={22} />
+                    <p className='font-bold'>6</p>
+                  </span>
+                  <span className='flex gap-2 items-center opacity-75'>
+                    <Clock width={22} />
+                    <p className='font-bold'>16:00 - 17:00</p>
+                  </span>
+                </div>
+              </CardContent>
+            </div>
+            <Button className="m-4 ml-auto h-28 aspect-square flex flex-col gap-4" variant="outline">
+              <Play />
+              Kezdés
+            </Button>
+          </Card>
+          <Textarea placeholder="Jegyzetek" className="h-28 max-h-48" />
           <ScrollArea className="h-[calc(100%-3rem)] pb-12">
             <DataTable columns={columns} data={data} className={"w-full h-full"} />
           </ScrollArea>
         </div>
       </div>
-      <div className='bg-primary-foreground rounded-xl p-4 h-full'>
-        <h2 className='text-center mb-4'>Üzenetek</h2>
+
+
+      <div className='bg-primary-foreground rounded-xl p-4 h-full row-span-2'>
+        <h2 className='md:text-left text-center mb-4'>Üzenetek</h2>
         <ScrollArea className="h-[calc(100%-3rem)]">
             <AssignmentMessage />
             <NotificationMessage />
         </ScrollArea>
       </div>
+
+
       <div className='bg-primary-foreground rounded-xl p-4'>
-        <h2 className='text-center mb-2'>Segédletek</h2>
+        <h2 className='md:text-left text-center mb-2'>Segédletek</h2>
         <ScrollArea className='h-16 overflow-x-auto gap-2 py-2'>
           <div className='flex w-max gap-2'>
             {courses.map(e => <Button variant="outline" key={e} className="font-bold text-xl"><Folder strokeWidth={4} /> {e}</Button>)}

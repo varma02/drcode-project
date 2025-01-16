78166dd66789f885d5db9b9ebf7d0231620729d3
varma02 <varma02.marci@gmail.com>
2025-01-16 19:41:00 +0000

    are you sure alert component

---
+++ web/src/components/AreYouSureAlert.jsx
@@ -0,0 +1,34 @@
+import { Trash } from "lucide-react";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./ui/alert-dialog";
+import { Button } from "./ui/button";
+
+export default function AreYouSureAlert({ 
+  title, description, trigger, cancelText, confirmText, onConfirm, onCancel
+}) {
+  
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        {trigger || <Button variant="outline" className="hover:bg-destructive">
+          <Trash /> Törlés
+        </Button>}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title || "Biztosan tötölni szeretnéd?"}</AlertDialogTitle>
+          <AlertDialogDescription>
+            {description || "A kiválasztott elemek és a velük kapcsolatos minden adat törlődik. Ezt a műveletet nem lehet visszavonni!"}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={onCancel}>
+            {cancelText || "Mégse"}
+          </AlertDialogCancel>
+          <AlertDialogAction variant="destructive" onClick={onConfirm}>
+            {confirmText || "Törlés"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}

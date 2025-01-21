54c9a74bbd0cafbd996fd6e297e39f14f740d7ad
varma02 <varma02.marci@gmail.com>
2025-01-21 21:40:56 +0000

    minor changes

--- server/src/lib/errorHandler.ts
+++ server/src/lib/errorHandler.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { BadRequestError, BaseError, ErrorCode, FieldsInvalidError } from './errors';
+import { BadRequestError, BaseError, ErrorCode, FieldsInvalidError, NotFoundError } from './errors';
 import { ResponseError } from 'surrealdb';
 
 export default function errorHandler(func : express.RequestHandler) {
@@ -12,13 +12,12 @@ export default function errorHandler(func : express.RequestHandler) {
         code = err.code;
         message = err.message;
       } else if (err instanceof ResponseError) {
-        if (
-          err.message.match(/Found.+for field.+with record.+but/gmi) ||
-          err.message.match(/Expected a single result output when using the ONLY keyword/gmi)
-        ) {
+        if (err.message.match(/Found.+for field.+with record.+but/gmi)) {
           err = new FieldsInvalidError();
         } else if (err.message.match(/The query was not executed due to a failed transaction/gmi)) {
           err = new BadRequestError("One or more fields are invalid or missing.");
+        } else if (err.message.match(/Expected a single result output when using the ONLY keyword/gmi)) {
+          err = new NotFoundError();
         } else {
           console.trace(err);
         }

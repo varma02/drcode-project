813eaa669cc940534da7d58303b701512c0736dc
MaalnaKeX <indomaalnakex@gmail.com>
2025-04-08 17:05:59 +0000

    Updated LessonCard

--- web/src/components/LessonCardItem.jsx
+++ web/src/components/LessonCardItem.jsx
@@ -27,45 +27,49 @@ export const LessonCardItem = ({course = "Scratch", teachers = ["Legfőbb Ügyé
     //   </CardFooter>
     // </Card>
     <Dialog>
-      <DialogTrigger className='w-full'>
-        <div variant="outline" className='flex gap-2 border rounded-lg p-2 cursor-pointer hover:bg-[#2a2a30] transition-all relative'>
+      <DialogTrigger>
+        <div variant="outline" className='w-max flex gap-2 border rounded-lg p-2 cursor-pointer hover:bg-[#2a2a30] transition-all relative'>
           <div className='flex flex-col'>
             <img src="https://seeklogo.com/images/S/scratch-cat-logo-7F652C6253-seeklogo.com.png" className='max-w-8 max-h-8 object-cover object-center rounded-lg mt-2' />
             {/* <img src="https://seeklogo.com/images/S/scratch-cat-logo-7F652C6253-seeklogo.com.png" className='max-w-8 max-h-8 object-cover object-center rounded-lg mt-2' /> */}
           </div>
-          <div className='w-full'>
-            <p className='text-left text-xl'>Scratch</p>
-            <div className='flex'>
-              {
-                teachers.map(e => 
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Avatar className="-mr-4 border-4 border-background h-10 rounded-full w-auto">
-                        <AvatarImage src="https://uploads.dailydot.com/2024/07/wet-owl-1.jpg?auto=compress&fm=pjpg" className="rounded-full" />
-                        <AvatarFallback>BI</AvatarFallback>
-                      </Avatar>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <p>{e}</p>
-                    </TooltipContent>
-                  </Tooltip>
-                )
-              }
+          <div className='flex gap-2'>
+            <div>
+              <p className='text-left text-xl'>Scratch</p>
+              <div className='flex'>
+                {
+                  teachers.map(e => 
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Avatar className="-mr-4 border-4 border-background h-10 rounded-full w-auto">
+                          <AvatarImage src="https://uploads.dailydot.com/2024/07/wet-owl-1.jpg?auto=compress&fm=pjpg" className="rounded-full" />
+                          <AvatarFallback>BI</AvatarFallback>
+                        </Avatar>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>{e}</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  )
+                }
+              </div>
             </div>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <div className='w-32 flex items-center gap-1'>
-                  <MapPin className='min-w-max' size={16} />
-                  <p className='truncate'>{location}</p>
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>{location}</p>
-              </TooltipContent>
-            </Tooltip>
-            <p className='flex items-center gap-1 max-w-full my-1'><Clock size={16} /> {time_start} - {time_end}</p>
-            {/* <p className='flex items-center absolute bottom-6 left-2.5'><User2 size={16} /> 11</p> */}
-            <p className='flex items-center'><User2 size={16} /> 11</p>
+            <div>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <div className='w-32 flex items-center gap-1'>
+                    <MapPin className='min-w-max' size={16} />
+                    <p className='truncate'>{location}</p>
+                  </div>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{location}</p>
+                </TooltipContent>
+              </Tooltip>
+              <p className='flex items-center gap-1 max-w-full my-1'><Clock size={16} /> {time_start} - {time_end}</p>
+              {/* <p className='flex items-center absolute bottom-6 left-2.5'><User2 size={16} /> 11</p> */}
+              <p className='flex items-center'><User2 size={16} /> 11</p>
+            </div>
           </div>
         </div>
         {/* <div variant="outline" className='flex gap-2 justify-between items-center font-bold border rounded-lg p-2 cursor-pointer hover:bg-[#2a2a30] transition-all'>

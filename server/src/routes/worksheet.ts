c66feffa42a975390aafe95824c905e16298ee5e
varma02 <varma02.marci@gmail.com>
2025-05-01 17:21:32 +0000

    /worksheet/create

--- server/src/routes/worksheet.ts
+++ server/src/routes/worksheet.ts
@@ -1,20 +1,18 @@
 import db from '../database/connection';
+import { DBRole } from '../database/models';
 import errorHandler from '../lib/errorHandler';
+import { UnauthorizedError } from '../lib/errors';
 import { PermissionDefaults, Thing } from '../lib/thing';
 import { respond200, validateRequest } from '../lib/utils';
-import ensureAuth from '../middleware/ensureauth';
-
-const isnotadmin = "array::find($user.roles, \"adminisrator\") IS NONE"
-const mineoradmin = {general: `($user.id == $employee OR NOT (${isnotadmin}))`}
 
 const worksheet = new Thing({
   table: "worked_at",
   permissions: {
-    create: PermissionDefaults.adminOnly,
-    getAll: PermissionDefaults.adminOnly,
-    getById: mineoradmin,
-    update: {general: `${mineoradmin.general} AND NOT (${isnotadmin} AND NOT ($fields.paid IS NONE))`},
-    remove: mineoradmin,
+    create: PermissionDefaults.noone,
+    getAll: PermissionDefaults.noone,
+    getById: PermissionDefaults.everyone,
+    update: {general: `(type::thing($user.id) == in AND !$fields.paid) OR (array::find($user.roles, "administrator") IS NOT NONE)`},
+    remove: {general: `(type::thing($user.id) == in) OR (array::find($user.roles, "administrator") IS NOT NONE)`},
   },
   fields: {
     start: {CONVERTER: "type::datetime($field)"},
@@ -28,14 +26,28 @@ worksheet.addDefaults({
   getById: false,
 });
 
+worksheet.router.post('/create', errorHandler(async (req, res) => {
+  validateRequest(req, `POST /worksheet/create`);
+  let { employee, out, start, end } = req.body;
+  if (req.user?.roles?.includes(DBRole.Admin) === false && employee)
+    throw new UnauthorizedError("The employee property is only available to admins");
+  if (!employee)
+    employee = req.user?.id;
+  const result = await db.query(`
+    RELATE ONLY (type::thing($employee))->worked_at->(type::thing($out)) CONTENT {
+      start: type::datetime($start),
+      end: type::datetime($end),
+      paid: false
+    };
+  `, {employee, out, start, end});
+  respond200(res, "POST /worksheet/create", {worksheet: result[0]});
+}))
+
 worksheet.router.get("/get", worksheet.get({
   WHERE: (req) => {console.log(req.user); return`
-    in == type::thing($employee)
+    in == type::thing($user.id)
     AND ${req.query?.paid ? "true" : "paid == false"}
   `},
-  extraFields: (req) => ({
-    employee: req.query?.id
-  }),
   postProcess: (res) => ({ worksheet: res }),
 }));
 

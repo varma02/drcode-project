ab5089c41d31db81f9d2f6b92983b2e8db181751
MaalnaKeX <75688160+MaalnaKeX@users.noreply.github.com>
2025-05-21 15:32:53 +0000

    worksheet paid field now works

--- server/src/routes/worksheet.ts
+++ server/src/routes/worksheet.ts
@@ -47,7 +47,7 @@ worksheet.router.post('/create', errorHandler(async (req, res) => {
 worksheet.router.get("/get", worksheet.get({
   WHERE: (req) => {console.log(req.user); return`
     in == type::thing($user.id)
-    AND ${req.query?.paid ? "true" : "paid == false"}
+    AND paid == ${req.query?.paid}
   `},
   postProcess: (res) => ({ worksheet: res }),
 }));

52051ca93eccae7b333d329052a5a82c504517c3
MaalnaKeX <75688160+MaalnaKeX@users.noreply.github.com>
2025-05-28 18:19:07 +0000

    basic admin statistic page

--- web/src/routes/AdminHome.jsx
+++ web/src/routes/AdminHome.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart'
-import { Bar, BarChart, CartesianGrid, Label, Pie, PieChart, XAxis } from 'recharts'
 import { useAuth } from '@/lib/api/AuthProvider'
 import { getAll, getStats } from '@/lib/api/api'
+import { Album, Component, GraduationCap, LoaderCircle, MapPinned, MessageSquare, User } from 'lucide-react'
+import { Progress } from '@/components/ui/progress'
+import { Button } from '@/components/ui/button'
+import { Link } from 'react-router-dom'
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart'
+import { Pie, PieChart } from 'recharts'
 
 export default function AdminHome() {
   const auth = useAuth()
@@ -37,159 +41,118 @@ export default function AdminHome() {
     getStats(auth.token).then(resp => setStats(resp.data))
   }, [auth.token])
 
+  
+  if (!stats) return (
+    <div className='h-screen w-full bg-background flex items-center justify-center'>
+      <LoaderCircle className='animate-spin ' />
+    </div>
+  )
+
   console.log(stats)
 
-  const chartData = [
-    { year: "2020", wedo: 186, scratch: 80, web: 90, unity: 100 },
-    { year: "2021", wedo: 305, scratch: 200, web: 120, unity: 80 },
-    { year: "2022", wedo: 237, scratch: 120, web: 100, unity: 70 },
-    { year: "2023", wedo: 73, scratch: 190, web: 140, unity: 90 },
-    { year: "2025", wedo: 209, scratch: 130, web: 70, unity: 100 },
-    { year: "2025", wedo: 214, scratch: 140, web: 100, unity: 70 },
-  ]
+  const iconIsze = 48
 
-  const chartConfig = {
-    wedo: {
-      label: "WeDo",
-      color: "#47b60c",
+  const noData = {
+    employees: {
+      name: "Alkalmazott",
+      icon: <User size={iconIsze} />
     },
-    scratch: {
-      label: "Scratch",
-      color: "#00a38b",
+    groups: {
+      name: "Csoport",
+      icon: <Component size={iconIsze} />
     },
-    web: {
-      label: "Web",
-      color: "#cc4800",
+    invites: {
+      name: "Meghívó",
+      icon: <MessageSquare size={iconIsze} />
     },
-    unity: {
-      label: "Unity",
-      color: "#084ac4",
+    locations: {
+      name: "Helyszín",
+      icon: <MapPinned size={iconIsze} />
+    },
+    students: {
+      name: "Tanuló",
+      icon: <GraduationCap size={iconIsze} />
+    },
+    subjects: {
+      name: "Kurzus",
+      icon: <Album size={iconIsze} />
     },
   }
 
-  const chartData3 = [
-    { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-    { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-    { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
-    { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-    { browser: "other", visitors: 90, fill: "var(--color-other)" },
-  ]
-  
-  const chartConfig3 = {
-    visitors: {
-      label: "Visitors",
-    },
-    chrome: {
-      label: "Chrome",
-      color: "hsl(var(--chart-1))",
-    },
-    safari: {
-      label: "Safari",
-      color: "hsl(var(--chart-2))",
-    },
-    firefox: {
-      label: "Firefox",
-      color: "hsl(var(--chart-3))",
-    },
-    edge: {
-      label: "Edge",
-      color: "hsl(var(--chart-4))",
-    },
-    other: {
-      label: "Other",
-      color: "hsl(var(--chart-5))",
-    },
-  }
-  
-  const chartData2 = Object.values(teacherGroups).map((e, i) => (
-    {employee: e.name, groups: e.groups.length, fill: `hsl(${10*i} 76% 61%)`}
-  ))
+  if (!stats) return (
+    <div className='h-screen w-full bg-background flex items-center justify-center'>
+      <LoaderCircle className='animate-spin' />
+    </div>
+  )
 
-  const chartConfig2 = Object.values(teacherGroups).map(e => (
-    {[e.name]: {label: e.name, color: "hsl(var(--chart-5))"}}
-  ))
+  const chartData = Object.entries(stats.group_per_employee).map((e, i) => ({employee: e[0], groups: e[1], fill: `hsl(${i*10} 76% 61%)`}))
+  
+  const chartConfig = {...chartData.map((e, i) => (
+    {[e.employee]: {label:e.employee, color:`hsl(${i*10} 76% 61%)`}}
+  ))}
+
 
   return (
     <div className='size-full bg-primary-foreground p-4 rounded-xl flex flex-1 flex-col'>
-      <ChartContainer config={chartConfig} className="h-[200px] w-[400px]">
-        <BarChart accessibilityLayer data={chartData}>
-          <CartesianGrid vertical={false} />
-          <XAxis
-            dataKey="year"
-            tickLine={false}
-            tickMargin={10}
-            axisLine={false}
-          />
-          <ChartTooltip content={<ChartTooltipContent />} />
-          <ChartLegend content={<ChartLegendContent />} />
-          {
-            Object.keys(chartConfig).map(e => 
-              <Bar key={e} dataKey={e} fill={`var(--color-${e})`} radius={4} />
-            )
+      <div className='max-w-screen-xl md:w-full mx-auto p-4 space-y-4'>
+        <div className='w-full grid grid-cols-3 gap-2 xl:flex'>
+          { Object.entries(stats.number_of).filter(e => e[0] in noData).map((e, i) => (
+            <Link to={`/${e[0]}`} key={i} variant="outline" className="w-full h-max p-2 rounded-md border-2 border-border transition-all hover:bg-card hover:scale-105">
+              <div className='flex gap-2 size-full items-center'>
+                {noData[e[0]].icon}
+                <div>
+                  <b className='text-2xl'>{e[1]}</b>
+                  <p>{noData[e[0]].name}</p>
+                </div>
+              </div>
+            </Link>
+            ))
           }
-        </BarChart>
-      </ChartContainer>
+        </div>
 
-      <ChartContainer
-          config={{}}
-          className="aspect-square max-h-[250px] w-max"
-        >
-        <PieChart>
-          <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
-          <Pie
-            data={chartData2}
-            dataKey="groups"
-            nameKey="employee"
-            innerRadius={60}
-            strokeWidth={5}
+        <div>
+          <p className='mb-2'>Óraadások folyamata ({stats.number_of.lessons.past} a {stats.number_of.lessons.total}-ből)</p>
+          <div className='flex items-center gap-2'>
+            <p>0</p>
+            <Progress value={stats.number_of.lessons.past / stats.number_of.lessons.total * 100} />
+            <p>{stats.number_of.lessons.total}</p>
+          </div>
+        </div>
+        
+        <div className="flex gap-4 justify-between">
+          <div>
+            <h3 className='text-3xl'>Bevétel</h3>
+            <div>
+              <p>Összesen: <b className='text-green-500'>{stats.income.total} HUF</b></p>
+              <p>Ma: <b className='text-green-500'>{stats.income.today} HUF</b></p>
+              <p>Héten: <b className='text-green-500'>{stats.income.this_week} HUF</b></p>
+              <p>Hónapban: <b className='text-green-500'>{stats.income.this_month} HUF</b></p>
+            </div>
+          </div>
+          <div>
+            <h3 className='text-3xl'>Dolgozott órák</h3>
+            <div>
+              <p>Összesen: {stats.worked_hours.total}</p>
+              <p>Ma: {stats.worked_hours.today}</p>
+              <p>Héten: {stats.worked_hours.this_week}</p>
+              <p>Hónapban: {stats.worked_hours.this_month}</p>
+            </div>
+          </div>
+        </div>
+        
+        <div>
+          <ChartContainer
+            config={chartConfig}
+            className="mx-auto aspect-square max-h-[250px] z-50 pb-0 [&_.recharts-pie-label-text]:fill-foreground"
           >
-            <ChartLegend
-              content={<ChartLegendContent nameKey="employee" />}
-              className="-translate-y-2 flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
-            />
-            <Label
-            content={({ viewBox }) => {
-              if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                return (
-                  <text
-                    x={viewBox.cx}
-                    y={viewBox.cy}
-                    textAnchor="middle"
-                    dominantBaseline="middle"
-                  >
-                    <tspan
-                      x={viewBox.cx}
-                      y={(viewBox.cy || 0) - 5}
-                      className="fill-foreground font-bold"
-                    >
-                      Alkalmazottak
-                    </tspan>
-                    <tspan
-                      x={viewBox.cx}
-                      y={(viewBox.cy || 0) + 15}
-                      className="fill-muted-foreground"
-                    >
-                      csoportszáma
-                    </tspan>
-                  </text>
-                )
-              }
-            }}
-            />
-          </Pie>
-        </PieChart>
-      </ChartContainer>
+            <PieChart>
+              <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+              <Pie data={chartData} dataKey="groups" label nameKey="employee" />
+            </PieChart>
+          </ChartContainer>
+        </div>
 
-      {/* <ChartContainer
-          config={chartConfig2}
-          className="aspect-square max-h-[300px] w-max"
-        >
-          <PieChart>
-            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
-            <Pie data={chartData2} dataKey="groups" nameKey={"employee"} innerRadius={60} />
-          </PieChart>
-        </ChartContainer> */}
-
+      </div>
     </div>
   )
 }

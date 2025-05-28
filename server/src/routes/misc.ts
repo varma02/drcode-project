9f79a60e51e71c724fa4728da599af8944db5e06
varma02 <varma02.marci@gmail.com>
2025-05-28 11:00:09 +0000

    fix stats

--- server/src/routes/misc.ts
+++ server/src/routes/misc.ts
@@ -17,17 +17,17 @@ router.get('/stats', ensureAuth, ensureAdmin, async (req, res) => {
   const stats = await db.query<any[]>(`
     RETURN {
       "number_of": {
-        "employees": (SELECT count() FROM employee),
-        "groups": (SELECT count() FROM group),
-        "invites": (SELECT count() FROM invite),
+        "employees": count(SELECT id FROM employee),
+        "groups": count(SELECT id FROM group),
+        "invites": count(SELECT id FROM invite),
         "lessons": {
-          "total": (SELECT count() FROM lesson),
-          "past": (SELECT count() FROM lesson WHERE start < time::now()),
-          "future": (SELECT count() FROM lesson WHERE start >= time::now()),
+          "total": count(SELECT id FROM lesson),
+          "past": count(SELECT id FROM lesson WHERE start < time::now()),
+          "future": count(SELECT id FROM lesson WHERE start >= time::now()),
         },
-        "locations": (SELECT count() FROM location),
-        "students": (SELECT count() FROM student),
-        "subjects": (SELECT count() FROM subject),
+        "locations": count(SELECT id FROM location),
+        "students": count(SELECT id FROM student),
+        "subjects": count(SELECT id FROM subject),
       },
       "income": {
         "total": 0,
@@ -36,10 +36,10 @@ router.get('/stats', ensureAuth, ensureAdmin, async (req, res) => {
         "this_month": 0,
       },
       "worked_hours": {
-        "total": (SELECT math::sum(end-start) FROM worked_at),
-        "today": (SELECT math::sum(end-start) FROM worked_at WHERE time::round(start, 1d) == time::round(time::now(), 1d)),
-        "this_week": (SELECT math::sum(end-start) FROM worked_at WHERE time::round(start, 1w) == time::round(time::now(), 1w)),
-        "this_month": (SELECT math::sum(end-start) FROM worked_at WHERE time::round(start, 1m) == time::round(time::now(), 1m)),
+        "total": math::sum(SELECT VALUE (duration::hours(end-start)) FROM worked_at),
+        "today": math::sum(SELECT VALUE (duration::hours(end-start)) FROM worked_at WHERE time::round(time::now(), 1d) == time::round(time::now(), 1d)),
+        "this_week": math::sum(SELECT VALUE (duration::hours(end-start)) FROM worked_at WHERE time::round(time::now(), 1w) == time::round(time::now(), 1w)),
+        "this_month": math::sum(SELECT VALUE (duration::hours(end-start)) FROM worked_at WHERE time::round(time::now(), 4w) == time::round(time::now(), 4w)),
       },
     }
   `)
